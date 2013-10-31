133497195072e3eac3d7d4764ef1631a3367bb79
David A. Mellis <d.mellis@arduino.cc>
2013-10-31 16:18:13 -0400

    Include RXTX 2.1-7 in the repository (files from Arduino).

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-javac A4S.java processing/src/Firmata.java
+javac -classpath RXTXcomm.jar A4S.java processing/src/Firmata.java
 mkdir -p org/firmata
 cp processing/src/*.class org/firmata/
 jar -cfm A4S.jar manifest.mf *.class org/firmata/*.class
